f629d5864a4c841170579268dd227c045b483013
timewarlock <506954185@qq.com>
2025-07-09 18:47:39 +0800

    添加角色能力集合 Hero Ability Set

--- Source/Asure/Private/MyDataAssets/StartUpData/DataAsset_HeroStartUPData.cpp
+++ Source/Asure/Private/MyDataAssets/StartUpData/DataAsset_HeroStartUPData.cpp
@@ -2,4 +2,26 @@
 
 
 #include "MyDataAssets/StartUpData/DataAsset_HeroStartUPData.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"	
 
+bool FWarriorHeroAbilitySet::IsValide() const
+{
+    return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UMyDataAsset_StartUPData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+	//遍历能力集
+	for (const FWarriorHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if (!AbilitySet.IsValide()) continue;
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+		InASCToGive->GiveAbility(AbilitySpec);
+		
+	}
+}
--- Source/Asure/Public/MyDataAssets/StartUpData/DataAsset_HeroStartUPData.h
+++ Source/Asure/Public/MyDataAssets/StartUpData/DataAsset_HeroStartUPData.h
@@ -4,8 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "MyDataAssets/StartUpData/DataAsset_StartUPDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUPData.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FWarriorHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Category = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorGameplayAbility> AbilityToGrant;
+	//检查AbilityToGrant是否有效
+	bool IsValide() const ;
+	
+	
+};
 /**
  * 
  */
@@ -13,5 +30,16 @@ UCLASS()
 class ASURE_API UMyDataAsset_StartUPData : public UDataAsset_StartUPDataBase
 {
 	GENERATED_BODY()
+
+
+public:
+	//重写基类里面的能力系统组件函数
+	virtual void GiveToAbilitySystemComponent(class UWarriorAbilitySystemComponent* InASCToGive, int ApplyLevel = 1) override
+		;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorHeroAbilitySet> HeroStartUpAbilitySets;
+
 	
 };
